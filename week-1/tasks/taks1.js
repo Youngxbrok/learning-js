da5778725911c073f292f27ff080af094a843cdd
Fernando Holguin Feliz <95173112+Youngxbrok@users.noreply.github.com>
2025-06-21 00:24:32 -0400

    Create taks1.js
    Primera tarea de la semana por revisión

---
+++ week-1/tasks/taks1.js
@@ -0,0 +1,16 @@
+let edad = 12;
+let suma;
+
+suma = edad + edad;
+
+console.log("la suma es: " + suma);
+
+if (edad >= 18) {
+  for (i = 1; i <= 10; i++) {
+    console.log(i + " Eres mayor de edad");
+  }
+} else {
+  for (i = 1; i <= 20; i++) {
+    console.log(i + " Eres menor de edad");
+  }
+}
