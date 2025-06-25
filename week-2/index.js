d3d2305e14751af8e005831ac52ce8dff00ffe9c
JosueIsOffline <josuehernandez2314@gmail.com>
2025-06-24 23:36:34 -0400

    chore: add exercise 'copyWithin' method

---
+++ week-2/index.js
@@ -0,0 +1,5 @@
+// Tienes un array que representa los niveles desbloqueados por un jugador en un videojuego:
+//
+// El jugador ahora encuentra un "portal" que le hace repetir los niveles 1, 2 y 3 pero empezando desde el índice 3 (es decir, los vuelve a jugar).
+
+let niveles = ["nivel1", "nivel2", "nivel3", "nivel4", "nivel5", "nivel6"];
