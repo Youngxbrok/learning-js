2ab666310b4636a7670748800d2cd23c8cea46a2
JosueIsOffline <josuehernandez2314@gmail.com>
2025-06-24 23:40:54 -0400

    fix: save and send day1.js file

---
+++ week-2/day1.js
@@ -0,0 +1,49 @@
+// Array methods
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Fresa", "Kiwi"];
+// const newArr = fruits.copyWithin(2, 1, 3);
+// console.log(newArr);
+// const str = "Pablito clavo una manutension a su esposa lavalprestol";
+// const str = "anaban"
+// const frequency = {};
+
+// for (let char of str) {
+//   frequency[char] = (frequency[char] || 0) + 1;
+// }
+// console.log(frequency);
+
+// for (i of fruits) {
+//   console.log(i);
+// }
+
+//toString() method
+// console.log(fruits.toString());
+
+//join() method
+// console.log(fruits.join(" "));
+
+//pop() method
+// console.log(fruits.pop());
+// console.log(fruits);
+
+//push() method
+// console.log(fruits.push("Fresa"));
+// console.log(fruits);
+
+//shift() method
+// console.log(fruits.shift());
+
+//unshift() method
+// console.log(fruits.unshift("Grape"));
+// console.log(fruits);
+
+// fruits[0] = "Otro fruit";
+// console.log(fruits);
+
+// const d = ["codk", "doj"];
+// const r = ["dsd", "sddf", "dfd"];
+
+//concat() method
+// const newOne = d.concat(r);
+// console.log(newOne);
+//
+//
