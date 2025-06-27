3eda22d64757fac11d5062545bdf3fcbf4d7ce5f
Fernando Holguin Feliz <95173112+Youngxbrok@users.noreply.github.com>
2025-06-27 19:26:04 -0400

    Mejora de la tarea

--- week-2/tasks/task-1.js
+++ week-2/tasks/task-1.js
@@ -11,6 +11,7 @@ const estudiantes = [
 
 // 1️⃣ MAP
 // Reto: Crea un nuevo array con los nombres de los estudiantes en mayúsculas.
+console.log("\n Creación de un array con los nombres de los estudiantes en mayúsculas \n ");
 const nombresEnMayusculas = [];
 
 const mayúsculas = estudiantes.map((student) => {
@@ -21,6 +22,7 @@ console.log(nombresEnMayusculas);
 
 // 2️⃣ FILTER
 // Reto: Filtra los estudiantes que tienen promedio mayor o igual a 7 (aprobados).
+console.log("\n Clasificación de los estudiantes que han aprobado \n ");
  const aprobados = estudiantes.filter((student) => {
    return student.promedio >= 7
  });
@@ -29,6 +31,7 @@ console.log(nombresEnMayusculas);
 
 // 3️⃣ SORT
 // Reto: Ordena el array original de estudiantes de mayor a menor según el promedio.
+console.log("\n Organización de los estudiantes según su promedio \n ");
 const ordenadosPorPromedio = estudiantes.sort((menor, mayor) => {
 
   return mayor.promedio - menor.promedio
@@ -39,6 +42,7 @@ console.log(ordenadosPorPromedio);
 
 // 4️⃣ REDUCE
 // Reto: Calcula el promedio global de toda la clase (promedio de promedios).
+console.log("\n Generación el promedio global de toda la clase \n ");
  const promedioClase = estudiantes.reduce((total, suma) => {
 
  return total + suma.promedio
@@ -49,6 +53,7 @@ console.log(ordenadosPorPromedio);
 
 // 5️⃣ FIND
 // Reto: Encuentra al estudiante con el nombre "Carlos".
+console.log("\n Busqueda del estudiante Carlos \n ");
  const carlos = estudiantes.find((student) => {
 
 return student.nombre ===  'Carlos';
@@ -61,5 +66,5 @@ return student.nombre ===  'Carlos';
 // Reto: Imprime en consola un resumen de cada estudiante con el formato:
 // "Ana tiene 21 años y un promedio de 8.5"
 
+console.log("\n Array con formato de texto \n");
  estudiantes.forEach((student) => console.log(`${student.nombre} tiene ${student.edad} años y un promedio de ${student.promedio} `));
- 
